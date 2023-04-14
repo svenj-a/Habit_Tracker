5f319d8192f8a1f3a6c1c2d36481cbf7f4e59840
Svenja <98711965+svenj-a@users.noreply.github.com>
2023-04-14 12:59:08 +0200

    Create users.py

---
+++ users.py
@@ -0,0 +1,14 @@
+class User:
+  
+  def __init__(self):
+    pass
+  
+  def create_profile(self, name):
+    pass
+  
+  def delete_profile(self, name):
+    pass
+  
+  def edit_profile(self, name):
+    pass
+  
